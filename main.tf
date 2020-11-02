097101241fd06c8781cd4d4b8561c6f3d5219580
geortzik <tzikas.nn@gmail.com>
2020-11-02 12:02:01 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,36 +3,36 @@
 terraform {
   required_version = ">=0.12.13"
   backend "s3" {
-	bucket = "myuniquetfk3ydumbucket"
-	key    = "terraform.tfstate"
-	region = "eu-central-1"
-	dynamodb_table = "myuniqueddbdumtable"
-	encrypt        = "true"
+    bucket         = "myuniquetfk3ydumbucket"
+    key            = "terraform.tfstate"
+    region         = "eu-central-1"
+    dynamodb_table = "myuniqueddbdumtable"
+    encrypt        = "true"
   }
 }
 
 #Download AWS provider
 provider "aws" {
-  region  = "eu-central-1"
+  region = "eu-central-1"
 }
 
 #Build an s3 bucket to store TF state
 resource "aws_s3_bucket" "state_bucket" {
   bucket = "myuniquetfk3ydumbucket"
 
-#Encrypt the s3 bucket
-server_side_encryption_configuration {
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
+  #Encrypt the s3 bucket
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
     }
   }
-}
 
-#Keep a version history of the state file
-versioning {
-  enabled = true
-}
+  #Keep a version history of the state file
+  versioning {
+    enabled = true
+  }
 
 }
 
@@ -40,17 +40,17 @@ versioning {
 resource "aws_dynamodb_table" "tf_lock_State" {
   name = "myuniqueddbdumtable"
 
-#Change the default billing mode
+  #Change the default billing mode
   billing_mode = "PAY_PER_REQUEST"
 
-#Hash key is required
+  #Hash key is required
   hash_key = "LockID"
 
-#Hash key must be an attribute
-attribute {
-  name = "LockID"
-  type = "S"
-}
+  #Hash key must be an attribute
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
 
 }
 
