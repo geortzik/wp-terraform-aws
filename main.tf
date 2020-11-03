5293bdaa77f71983587e652e1357d2eda702c440
geortzik <tzikas.nn@gmail.com>
2020-11-03 14:06:47 +0200

    Fix syntax warning

--- main.tf
+++ main.tf
@@ -64,7 +64,7 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "subnet1" {
-  vpc_id     = "${aws_vpc.main.id}"
+  vpc_id     = aws_vpc.main.id
   cidr_block = "190.160.1.0/24"
 
   tags = {
