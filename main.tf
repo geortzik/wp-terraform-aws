bf5f6f23d9e607bb93f2a7fc00a695e6be4fb780
geortzik <tzikas.nn@gmail.com>
2020-11-05 21:23:06 +0200

    Massive update main.tf
    This is basically our first complete attempt to create and configure our
    whole infrastructure. We add the resources we were lacking to create the
    proper connection between our lb and vm. We create the whole procedure
    to get a TLS certificate for our domain. We also generate a random
    password for our db instance for security reasons. Still a couple of
    issues though with the default subnets. Still missing Route53 A records.

--- main.tf
+++ main.tf
@@ -54,33 +54,43 @@ resource "aws_dynamodb_table" "tf_lock_State" {
 
 }
 
-resource "aws_vpc" "main" {
-  cidr_block       = "190.160.0.0/16"
-  instance_tenancy = "default"
-
+#Adopt default VPC into management
+resource "aws_default_vpc" "default" {
   tags = {
-    Name = "main"
+    Name = "Default VPC"
   }
 }
 
-resource "aws_subnet" "subnet1" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "190.160.1.0/24"
+#Adopt default subnet1 into managment
+resource "aws_default_subnet" "default_subnet1" {
+  availability_zone = "eu-central-1a"
 
   tags = {
-    Name = "Subnet1"
+    Name = "Default subnet for eu-central-1a"
   }
 }
 
+#Adopt default subnet2 into managment
+resource "aws_default_subnet" "default_subnet2" {
+  availability_zone = "eu-central-1b"
+
+  tags = {
+    Name = "Default subnet for eu-central-1b"
+  }
+}
+
+#Create vm instance vm1
 resource "aws_instance" "vm1" {
-  ami             = "ami-0c960b947cbb2dd16"
-  instance_type   = "t2.micro"
-  security_groups = [aws_security_group.vm1_sg.id]
+  ami                     = "ami-0c960b947cbb2dd16"
+  instance_type           = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.vm1_sg.id]
 }
 
-resource "aws_security_group" "elb_sg" {
-  name        = "elb_sg"
-  description = "Allow all inbound traffic to ELB"
+#Create security group for the load balancer
+resource "aws_security_group" "lb_sg" {
+  name        = "lb_sg"
+  description = "Allow all inbound traffic to LB"
+  vpc_id      = aws_default_vpc.default.id
 
   ingress {
     description = "HTTPS"
@@ -107,16 +117,18 @@ ingress {
   }
 }
 
+#Create security group for the vm instance
 resource "aws_security_group" "vm1_sg" {
   name        = "vm1_sg"
-  description = "Allow HTTP inbound traffic from elb"
+  description = "Allow HTTP inbound traffic from lb"
+  vpc_id      = aws_default_vpc.default.id
 
   ingress {
-    description = "HTTP from elb"
+    description = "HTTP from lb"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [aws_default_vpc.default.cidr_block]
   }
 
 ingress {
@@ -135,35 +147,121 @@ ingress {
   }
 }
 
-resource "aws_elb" "elb1" {
-  name               = "elb1"
-  availability_zones = ["eu-central-1b", "eu-central-1a"]
-  security_groups    = [aws_security_group.elb_sg.id]
-  instances          = [aws_instance.vm1.id]
+#Create load balancer target group
+resource "aws_lb_target_group" "lbtg" {
+  name     = "lbtg1"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_default_vpc.default.id
+}
 
-#  listener {
-#    instance_port     = 80
-#    instance_protocol = "http"
-#    lb_port           = 80
-#    lb_protocol       = "http"
-#  }
+#Create load balancer target group attachment
+resource "aws_lb_target_group_attachment" "lbatt" {
+  target_group_arn = aws_lb_target_group.lbtg.arn
+  target_id        = aws_instance.vm1.id
+  port             = 80
+}
 
-  listener {
-    instance_port      = 80
-    instance_protocol  = "http"
-    lb_port            = 443
-    lb_protocol        = "https"
+#Create load balancer
+resource "aws_lb" "lb1" {
+  name                = "lb1"
+#  availability_zones  = ["eu-central-1b", "eu-central-1a"]
+  load_balancer_type  = "application"
+  security_groups     = [aws_security_group.lb_sg.id]
+  subnets             = ["aws_default_subnet.default_subnet1.id", "aws_default_subnet.default_subnet2.id"]
+
+}
+
+#Create load balancer listener
+resource "aws_lb_listener" "lb1_listener" {
+  certificate_arn   = aws_acm_certificate_validation.default.certificate_arn
+  load_balancer_arn = aws_lb.lb1.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.lbtg.arn
   }
 }
 
+#Redirect action
+resource "aws_lb_listener_rule" "redirect_http_to_https" {
+  listener_arn = aws_lb_listener.lb1_listener.arn
+
+  action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+
+  condition {
+    http_header {
+      http_header_name = "X-Forwarded-For"
+      values           = ["192.168.1.*"]
+    }
+  }
+}
+
+#Create a cert for our domain name
+resource "aws_acm_certificate" "default" {
+  domain_name               = "www.aws-gt.nkorb.gr"
+#  subject_alternative_name  = "aws-gt.nkorb.gr"
+  validation_method         = "DNS"
+
+  lifecycle {
+    create_before_destroy   = true
+  }
+}
+
+#Create route53 CNAME record for validation
+resource "aws_route53_record" "validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.default.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id = "Z020595615Z5MST8DWA0V"
+}
+
+#Successful validation
+resource "aws_acm_certificate_validation" "default" {
+  certificate_arn          = aws_acm_certificate.default.arn
+
+  validation_record_fqdns = [for record in aws_route53_record.validation : record.fqdn]
+}
+
+#Create a random password for RDS db instance
+resource "random_password" "password" {
+  length = 16
+  special = true
+  override_special = "%@/_"
+}
+
+#Create an RDS db instance
 resource "aws_db_instance" "db_1" {
   allocated_storage    = 20
 # storage_type         = "gp2"
   engine               = "mysql"
   instance_class       = "db.t2.micro"
   name                 = "db1"
-# username             = "foo"
-# password             = "foobarbaz"
+  username             = "foo"
+  password             = random_password.password.result
   port                 = 3306
   availability_zone    = "eu-central-1b"
 }
+
+
